5f0e4d400f657f4ae80659fdfc304f2b0fbd9672
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-08-28 22:01:58 -0600

    Move the alias of Credentials to make available their functions on Users functions.

--- lib/cms_api/accounts/accounts.ex
+++ lib/cms_api/accounts/accounts.ex
@@ -7,6 +7,7 @@ defmodule CmsApi.Accounts do
   alias CmsApi.Repo
 
   alias CmsApi.Accounts.User
+  alias CmsApi.Accounts.Credential
 
   @doc """
   Returns the list of users.
@@ -58,6 +59,7 @@ defmodule CmsApi.Accounts do
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.insert()
   end
 
@@ -76,6 +78,7 @@ defmodule CmsApi.Accounts do
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.update()
   end
 
@@ -108,7 +111,6 @@ defmodule CmsApi.Accounts do
     User.changeset(user, %{})
   end
 
-  alias CmsApi.Accounts.Credential
 
   @doc """
   Returns the list of credentials.
