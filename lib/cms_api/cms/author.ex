fe95580eb0271921ffc5d6abddd3d3775cc4deea
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-08-28 23:18:00 -0600

    Add author table on CMS context and update their relationship with pages.

---
+++ lib/cms_api/cms/author.ex
@@ -0,0 +1,23 @@
+defmodule CmsApi.CMS.Author do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias CmsApi.CMS.Author
+
+
+  schema "authors" do
+    field :bio, :string
+    field :genre, :string
+    field :role, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Author{} = author, attrs) do
+    author
+    |> cast(attrs, [:bio, :role, :genre])
+    |> validate_required([:bio, :role, :genre])
+    |> unique_constraint(:user_id)
+  end
+end
