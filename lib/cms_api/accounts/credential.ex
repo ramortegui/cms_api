9543d338b9a76178e2647e7581ada6a3bd59bbba
Ruben Amortegui <ruben.amortegui@gmail.com>
2017-08-28 21:02:00 -0600

    Add credentials relationship to user account using the phx.gen.context command.

---
+++ lib/cms_api/accounts/credential.ex
@@ -0,0 +1,21 @@
+defmodule CmsApi.Accounts.Credential do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias CmsApi.Accounts.Credential
+
+
+  schema "credentials" do
+    field :email, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Credential{} = credential, attrs) do
+    credential
+    |> cast(attrs, [:email])
+    |> validate_required([:email])
+    |> unique_constraint(:email)
+  end
+end
